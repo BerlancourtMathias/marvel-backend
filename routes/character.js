f016d6424db27a45a73e9d79e3e1667fbdb37b49
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-19 17:42:58 +0100

    launch error fixed

--- routes/character.js
+++ routes/character.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const router = express.Router();
 const cors = require("cors");
 const morgan = require("morgan");
-
+const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
