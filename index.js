4864a9746dab715324c0f80227db08768e0b3d51
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-19 19:34:46 +0100

    add morgan

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 
 app.get("/", async (req, res) => {
   res.status(200).json({ message: "connexion ok ✔️" });
