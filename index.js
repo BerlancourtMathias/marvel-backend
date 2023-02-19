0927e3d85f5ee916ff790b2306cab5e3a54d4abe
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-19 18:50:56 +0100

    comment on some lines

--- index.js
+++ index.js
@@ -25,21 +25,21 @@ app.use(comicsRoutes);
 // console.log("process.env.API_URL", process.env.API_URL);
 // console.log("process.env.PORT:", process.env.PORT);
 
-app.get("/comics", async (req, res) => {
-  console.log(req.query);
-  const skip = req.query.skip || "0";
-  const limit = req.query.limit || "100";
-  const title = req.query.title || "";
-  try {
-    const response = await axios.get(
-      `${process.env.API_URL}/comics?apiKey=${process.env.API_KEY}&skip=${skip}&limit=${limit}&title=${title}`
-    );
+// app.get("/comics", async (req, res) => {
+//   console.log(req.query);
+//   const skip = req.query.skip || "0";
+//   const limit = req.query.limit || "100";
+//   const title = req.query.title || "";
+//   try {
+//     const response = await axios.get(
+//       `${process.env.API_URL}/comics?apiKey=${process.env.API_KEY}&skip=${skip}&limit=${limit}&title=${title}`
+//     );
 
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+//     res.status(200).json(response.data);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// });
 
 app.all("*", (req, res) => {
   res.status(404).json({ message: "Cette route n'existe pas" });
