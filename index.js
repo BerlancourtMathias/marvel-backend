30a4b71c089c9fc490960e472dcc650e027d53ab
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-19 20:03:08 +0100

    remove ||

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "Cette route n'existe pas" });
 });
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`started`);
 });
